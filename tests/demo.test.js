8da82838e48298a27962f891a4c734dee8251d84
Josh Johnston <josh@x-team.com>
2024-03-20 16:44:18 +1100

    failing test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -2,6 +2,6 @@ const { getTrue } = require('../src/good-functions')
 
 describe('Demo', () => {
   it('passes the test', () => {
-    expect(getTrue()).toBe(true)
+    expect(getTrue()).toBe(false)
   })
 })
