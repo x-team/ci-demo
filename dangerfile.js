98e68fa5fa9ee74d49f200373a5d54da0ec50a22
Josh Johnston <josh@x-team.com>
2024-03-21 17:41:51 +1100

    fix bug in dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -9,4 +9,4 @@ schedule(istanbulCoverage({
   }
 }))
 
-markdown(`[Coverage Report](https://ci-demo-builds.s3.us-east-1.amazonaws.com/${CODEBUILD_WEBHOOK_TRIGGER}/lcov-report/index.html))
+markdown(`[Coverage Report](https://ci-demo-builds.s3.us-east-1.amazonaws.com/${process.env.CODEBUILD_WEBHOOK_TRIGGER}/lcov-report/index.html)`)
